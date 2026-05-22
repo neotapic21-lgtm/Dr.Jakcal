ad6a2693b8145cfc2aef2fd209238644abed6ec7
neotapic21-lgtm <neotapic21@gmail.com>
2026-05-22 20:45:18 +0800

    s

--- LAV-1.c
+++ LAV-1.c
@@ -4,21 +4,21 @@ int main(void) {
 
     FILE *fp;
     char name[100];
-    char section[100];
     char program[100];
+    char section[100];
     int age;
 
     sprintf(name, "Neo Tapic");
     age = 20;
-    sprintf(section, "1-1");
     sprintf(program, "Diploma in Information Technology");
+    sprintf(section, "1-1");
 
     fp = fopen("Neo_Tapic.txt", "w");
 
-    fgets(name, sizeof(name), fp);
-    fscanf(fp, "%d", &age);
-    fscanf(fp, "%s", section);
-    fgets(program, sizeof(program), fp);
+    fprintf(fp, "%s\n", name);
+    fprintf(fp, "%d\n", age);
+    fprintf(fp, "%s\n", program);
+    fprintf(fp, "%s\n", section);
 
     fclose(fp);
     
@@ -27,8 +27,8 @@ int main(void) {
     
     printf("Name: %s\n", name);
     printf("Age: %d\n", age);
-    printf("Section: %s\n", section);
     printf("Program: %s\n", program);
+    printf("Section: %s\n", section);
 
     fclose(fp);
 
