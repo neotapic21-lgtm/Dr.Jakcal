274aff0858a44a6b6854950911f587b4773b1c0e
neotapic21-lgtm <neotapic21@gmail.com>
2026-05-23 10:29:39 +0800

    dsaw

--- LAV-1.c
+++ LAV-1.c
@@ -4,28 +4,27 @@ int main() {
 
     FILE *fp;
     char name[100];
-    char course[100], section [100];
+    char coursesection [100];
     int age;
 
     sprintf(name, "Neo Tapic");
     age = 20;
-    sprintf(course, "DIT 1-1");
+    sprintf(coursesection, "DIT 1-1");
 
     fp = fopen("    Neo_Tapic.txt", "w");
 
     fgets(name, sizeof(name), fp);
     fscanf(fp, "%d", &age);
-    fgets(course, sizeof(course), fp);
-    fscanf(fp, "%s", section);
+    fgets(coursesection, sizeof(coursesection), fp);
 
     fclose(fp);
     
     fp = fopen("Neo_Tapic.txt", "r");
-    fscanf(fp, "%s %d %s %s", name, &age, course, section);
+    fscanf(fp, "%s %d %s %s", name, &age, coursesection);
     
     printf("Name: %s\n", name);
     printf("Age: %d\n", age);
-    printf("Program And Section: %s\n", course, section);
+    printf("Program And Section: %s\n", coursesection);
 
     fclose(fp);
 
