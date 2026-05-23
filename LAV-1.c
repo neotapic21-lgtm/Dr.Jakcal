ce9fbb8a2defe70043075025a78ddd7a32f9bdd4
neotapic21-lgtm <neotapic21@gmail.com>
2026-05-23 10:25:18 +0800

    poop

--- LAV-1.c
+++ LAV-1.c
@@ -4,28 +4,28 @@ int main() {
 
     FILE *fp;
     char name[100];
-    char program[100], section [100];
+    char course[100], section [100];
     int age;
 
     sprintf(name, "Neo Tapic");
     age = 20;
-    sprintf(program, "DIT 1-1");
+    sprintf(course, section, "DIT 1-1");
 
     fp = fopen("Neo_Tapic.txt", "w");
 
     fgets(name, sizeof(name), fp);
     fscanf(fp, "%d", &age);
-    fgets(program, sizeof(program), fp);
+    fgets(course, sizeof(course), fp);
     fscanf(fp, "%s", section);
 
     fclose(fp);
     
     fp = fopen("Neo_Tapic.txt", "r");
-    fscanf(fp, "%s %d %s %s", name, &age, program, section);
+    fscanf(fp, "%s %d %s %s", name, &age, course, section);
     
     printf("Name: %s\n", name);
     printf("Age: %d\n", age);
-    printf("Program And Section: %s\n", program, section);
+    printf("Program And Section: %s\n", course, section);
 
     fclose(fp);
 
