6f7a2cc15da57777e4f7731a0f4cf11ca05bbed8
neotapic21-lgtm <neotapic21@gmail.com>
2026-06-13 00:42:17 +0800

    das

--- practice2.c
+++ practice2.c
@@ -10,7 +10,7 @@ int main(){
     ptr = &num;
 
     printf("Enter a value: %d", num);
-    printf("Address: %p", (void)num);
+    printf("Address: %p", (void*)num);
     printf("Value using pointer: %d", *ptr);
 
     return 0;
